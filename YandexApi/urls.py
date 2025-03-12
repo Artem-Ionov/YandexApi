d4e9fe70d59052fb3cd7f1b0af8c8aacbdc78c43
Artem <ionov96artem@mail.com>
2025-03-12 21:34:40 +0400

    Add base.html

--- YandexApi/urls.py
+++ YandexApi/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('App.urls'))
+    path('', include('App.urls')),                          # Включение списка url из указанной директории
+    path('accounts/', include('django.contrib.auth.urls'))  # Включение авторизации
 ]
