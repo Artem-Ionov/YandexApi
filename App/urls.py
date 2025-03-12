bf0f5af28490cbf80a181aa5f44343a68a9dbd8e
Artem <ionov96artem@mail.com>
2025-03-12 20:13:23 +0400

    Add download_file in urls, views and templates

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import file_list
+from .views import file_list, download_file
 
 urlpatterns = [
     path('', file_list, name='file_list'),
+    path('download/<path:pk>', download_file, name='download_file'),
 ]
--- App/views.py
+++ App/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
-                                                                      
+'''
+Для написания данного модуля использовалась документация по REST API: 
+https://yandex.ru/dev/disk-api/doc/ru/reference/public
+'''                                                                      
 def file_list(request):
-    '''
-    Функция для получения списка файлов на Яндекс диске
-    Для её написания использовалась документация по REST API: https://yandex.ru/dev/disk-api/doc/ru/reference/public
-    '''                   
-    # Запрос на получение метаинформации опубликованного ресурса 
-    Meta_info = 'https://cloud-api.yandex.net/v1/disk/public/resources'       
+    'Функция для получения списка файлов на Яндекс диске'        
+
+    # Берём из документации
+    meta_info = 'https://cloud-api.yandex.net/v1/disk/public/resources'       
 
     # Получаем публичную ссылку как параметр введённого GET-запроса
     public_link = request.GET.get('public_link')
@@ -17,7 +18,7 @@ def file_list(request):
         return HttpResponse('Требуется публичная ссылка', status=400)
     
     # Запрос на получение страницы с помощью библиотеки requests
-    response = requests.get(f"{Meta_info}?public_key={public_link}")
+    response = requests.get(f"{meta_info}?public_key={public_link}")
     # Если запрос не успешен, выводим ошибку с соответствующим кодом
     if response.status_code!=200:
         return HttpResponse('Ошибка извлечения файла из Яндекс диска', status=response.status_code)
@@ -27,4 +28,26 @@ def file_list(request):
 
     # Выводим на экран информацию с помощью html-файла, в который передаём необходимые данные в словаре
     return render(request, 'file_list.html', {'files':files})
+
+
+def download_file(request, pk):
+    'Функция для загрузки файла с Яндекс диска'
+
+    # Берём из документации
+    download = 'https://cloud-api.yandex.net/v1/disk/public/resources/download'
+
+    # Используем public_key опубликованного ресурса
+    response = requests.get(f"{download}?public_key={pk}")
+    # Если запрос не успешен, выводим ошибку с соответствующим кодом
+    if response.status_code!=200:
+        return HttpResponse('Ошибка извлечения файла из Яндекс диска', status=response.status_code)
+    
+    # Получение ссылки на скачивание файла
+    link = response.json().get('href', '')
+    # Запрос на скачивание 
+    response2 = requests.get(f"{link}")
+
+    return response2
+
+
 
